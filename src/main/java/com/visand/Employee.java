ec92fc4905d3c4a8a5b3b0fa4327c58eed0bf719
rinkevics <rinkevics@gmail.com>
2019-04-04 02:50:02 +0300

    setup

--- src/main/java/com/visand/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.visand;
-
-public class Employee {
-    public String name;
-
-    public Employee(String name) {
-        this.name = name;
-    }
-}
--- src/main/java/com/visand/MyService.java
+++
@@ -1,8 +0,0 @@
-package com.visand;
-
-public class MyService {
-
-    public String message() {
-        return "hi2";
-    }
-}
